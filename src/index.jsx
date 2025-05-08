9a478485038958bab0e663f66dd1d07f125b5727
asym1 <asym17182@gmail.com>
2025-05-09 00:31:24 +0400

    First React Website!
    react fun facts

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,29 @@ const root = createRoot(document.getElementById("root"))
 function Page() {
     return (
         <ol> 
-          <li> Hi</li>
-          <li> Hi</li>
-          <li> Hi </li>
-          <li> Hi </li>
-          <li> Hi </li>
+          <li>React is a popular library, so I will be able to
+            fit in with all the coolest devs out there! 😎</li>
+            <li>I am more likely to get a job as a front end developer
+            if I know React</li>
         </ol>
     );
 }
 
+function Header() {
+  return (
+    <img src="src/assets/react.svg" width="40" alt="react logo"/>
+  );
+}
+
+function Footer() {
+  return (
+    <footer> <small> 2024 copyright Fortnite Vbucks </small></footer>
+  )
+}
 root.render(
-  < Page />
+  <main>
+    <Header />
+    <Page />
+    <Footer />
+  </main>
 )
