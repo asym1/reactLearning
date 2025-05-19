45819e1033c8e64dd92c5ce41fd8f7e3665793f1
asym1 <asym17182@gmail.com>
2025-05-19 15:16:51 +0400

    Added the header of the new travel journal project

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="h">
+            <img src="src/assets/Globe.png" alt="globeIcon" className="logo" />
+            <text className="webtitle"> my travel journal.</text>
+        </header>
+    );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,4 @@ import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 const root = createRoot(document.getElementById("root"))
 
-root.render(
-  <App />
-)
+root.render( <App /> )
