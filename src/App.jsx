5f2fe9eee80e1901a9fc941daa063f44cf651e0c
asym1 <asym17182@gmail.com>
2025-08-19 17:21:52 +0400

    adding key prop to entry components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Entry from "./components/Entry"
 import data from "../data.js"
 
 export default function App() { //parent component
-    const dataMapped = data.map((element) => {return <Entry img={element.img} title={element.title} country={element.country}
+    const dataMapped = data.map((element) => {return <Entry 
+    key={element.id} //this ensures react is able to understand the order and position of elements
+    img={element.img} title={element.title} country={element.country}
     googleMapsLink={element.googleMapsLink} dates={element.dates} text={element.text}/>})
     return (
       //children components
