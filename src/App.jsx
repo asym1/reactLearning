9e4aaa5f305494705000451490048104be344445
asym1 <asym17182@gmail.com>
2025-08-19 17:53:20 +0400

    Setup All files for Chef Claude Web App and final project
    This will be the project that will be put to production and uses AI API to show recipes based on ingredients

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import Header from "./components/Header"
-import Entry from "./components/Entry"
-import data from "../data.js"
 
 export default function App() { //parent component
-    const dataMapped = data.map((element) => {return <Entry 
-      key={element.id} //this ensures react is able to understand the order and position of elements
-      entry={element}/>})
-    return (
-      //children components
-      <>
-        <Header />
-        {dataMapped}
-      </>
+  return (
+    //children components
+    <>
+      <h1> Hello World</h1>
+    </>
     );
 }
--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header className="h">
-            <img src="src/assets/Globe.png" alt="globeIcon" className="logo" />
-            <text className="webtitle"> my travel journal.</text>
-        </header>
-    );
-}
