064d5f78088d028e6c5bd1d0c661b3301f4498db
asym1 <asym17182@gmail.com>
2025-09-10 12:50:14 +0400

    Updated Website with hardcoded list and form now adds to list but doesn't update website
    next is implementing React States

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,31 @@
 export default function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const mapped_ingredients = ingredients.map(x => <li> {x} </li>);
+
+    function onSubmitEvent(event) { /* event is the native web event passed on*/
+        event.preventDefault() // stops the page from adding stuff to URL & refreshing
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        mapped_ingredients.concat(<li> {newIngredient} </li>)
+        console.log(newIngredient)
+    }
+
     return (
-    <main>
-        <form className="add-ingredient-form">
-            <input 
-            aria-label="Add Ingredient"
-            type="text"
-            placeholder="e.g. Oregano"
-            />
-            <button>Add Ingredient</button>
-        </form>
-    </main>
+        /* Form That Triggers onSubmitEvent when it's clicked */
+        <main>
+            
+            <form className="add-ingredient-form" onSubmit={onSubmitEvent}>
+                <input 
+                    aria-label="Add Ingredient"
+                    type="text"
+                    placeholder="e.g. Oregano"
+                    name="ingredient"
+                />
+                <button >Add Ingredient</button> 
+            </form>
+            <ul>
+                {mapped_ingredients}
+            </ul>
+        </main>
     )
 }
