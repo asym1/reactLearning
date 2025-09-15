9c37701ffa38c91fa23d6ffe7953c6e8738a1404
asym1 <asym17182@gmail.com>
2025-09-15 15:23:43 +0400

    Updated Main component to update using state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,29 @@
+import React from 'react'
 export default function Main() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-    const mapped_ingredients = ingredients.map(x => <li> {x} </li>);
-
+    const [ingredientArray, updateArray] = React.useState([<ul></ul>])
     function onSubmitEvent(event) { /* event is the native web event passed on*/
         event.preventDefault() // stops the page from adding stuff to URL & refreshing
         const formData = new FormData(event.currentTarget)
         const newIngredient = formData.get("ingredient")
-        mapped_ingredients.concat(<li> {newIngredient} </li>)
-        console.log(newIngredient)
+        let updatedArray = ingredientArray.concat(<li> {newIngredient} </li>)
+        updateArray(updatedArray)
+        
     }
 
     return (
         /* Form That Triggers onSubmitEvent when it's clicked */
         <main>
-            
             <form className="add-ingredient-form" onSubmit={onSubmitEvent}>
                 <input 
-                    aria-label="Add Ingredients"
+                    aria-label="Add Ingredient"
                     type="text"
                     placeholder="e.g. Oregano"
                     name="ingredient"
                 />
-                <button> Add Ingredient </button> 
+                <button onSubmit={onSubmitEvent}> Add Ingredient </button>
             </form>
             <ul>
-                {mapped_ingredients}
+                {ingredientArray}
             </ul>
         </main>
     )
