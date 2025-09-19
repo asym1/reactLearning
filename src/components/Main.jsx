a252a743e7b01b42492a8d6a80dc8723b07cc5aa
asym1 <asym17182@gmail.com>
2025-09-19 11:19:07 +0400

    updated form to use action instead of onSubmit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 export default function Main() {
     const [ingredientArray, updateArray] = React.useState([<ul></ul>])
-    function onSubmitEvent(event) { /* event is the native web event passed on*/
-        event.preventDefault() // stops the page from adding stuff to URL & refreshing
-        const formData = new FormData(event.currentTarget)
+
+    function addIngredient(formData) { /* event is the native web event passed on*/
         const newIngredient = formData.get("ingredient")
         let updatedArray = ingredientArray.concat(<li> {newIngredient} </li>)
         updateArray(updatedArray)
-        
     }
 
     return (
         /* Form That Triggers onSubmitEvent when it's clicked */
         <main>
-            <form className="add-ingredient-form" onSubmit={onSubmitEvent}>
+            <form className="add-ingredient-form" action={addIngredient}>
                 <input 
                     aria-label="Add Ingredient"
                     type="text"
                     placeholder="e.g. Oregano"
                     name="ingredient"
                 />
-                <button onSubmit={onSubmitEvent}> Add Ingredient </button>
+                <button> Add Ingredient </button>
             </form>
             <ul>
                 {ingredientArray}
